9399c27e147956e621432746d0b4c69f95c01a30
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-05-03 01:03:10 +0300

    updated the views

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from ..request import get_Articles, get_articles_sos, get_headlines, get_sources
+from ..requests import get_articles, get_articles_sos, get_headlines, get_sources
 from . import main
 
 
@@ -8,15 +8,15 @@ def index():
 
     sources = get_sources()
     
-    articles = get_Articles(domain='bbc.com'+','+'cnn.com'+','+'techcrunch.com')
+    articles = get_articles('bbc-news')
     
-    return render_template('index.html',source=source,articles=articles)
+    return render_template('index.html',articles=articles)
 
 
 @main.route('/articles')
 def articles():
     
-    articles=get_Articles(domain='bbc.com'+','+'cnn.com'+','+'techcrunch.com')
+    articles=get_articles(domain='bbc.com'+','+'cnn.com'+','+'techcrunch.com')
    
     return render_template('articles.html',articles=articles)
 
@@ -38,14 +38,4 @@ def headlines():
     return render_template('articles.html',articles=articles)
 
 
-# @main.route('/sources/<a>')
-# def source_list(a):
-#     a=re.sub(r'[^\w]', ' ', a)
-#     a=a.strip()
 
-#     print(a)
-#     a=re.sub(' +', ' ', a)
-#     a=a.split(" ")
-#     joinsource='-'.join(a)
-#     articles=get_articles_sos(joinsource)
-#     return render_template('listfromsos.html',articles=articles)

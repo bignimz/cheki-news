3801c4457f5bfebe3119d88e5641eb8008d90970
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-05-02 22:12:03 +0300

    Added secret key and api key variables in the config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
 class Config:
     
-    NEWS_BASE_URL_SOURCES='https://newsapi.org/v2/top-headlines/sources?apiKey={}'
-    NEWS_BASE_EVERYTHING_URL='https://newsapi.org/v2/everything?domains={}&apiKey={}'
-    NEWS_BASE_HEADLINES_URL='https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
-    NEWS_BASE_SOURCE='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
-    
+    NEWS_BASE_URL_SOURCES ='https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+    NEWS_BASE_EVERYTHING_URL ='https://newsapi.org/v2/everything?domains={}&apiKey={}'
+    NEWS_BASE_HEADLINES_URL ='https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+    NEWS_BASE_SOURCE ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
 class ProdConfig(Config):
     pass
 
